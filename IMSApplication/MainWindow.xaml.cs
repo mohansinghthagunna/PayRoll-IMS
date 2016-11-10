7cc85ce7c658971a7ee194b0be9ed04a978cf225
Procit BV <Procit BV>
2016-11-10 20:21:14 +0545

    Project added by Kishor

--- IMSApplication/MainWindow.xaml.cs
+++ IMSApplication/MainWindow.xaml.cs
@@ -23,6 +23,12 @@ namespace IMSApplication
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            string a = "Hello";
         }
     }
 }
